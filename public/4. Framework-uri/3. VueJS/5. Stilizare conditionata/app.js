6ecbe97517abdb3dc8716da3c2475939bbd5d6fc
Sergiu <chilatsergiu@gmail.com>
2021-08-19 18:02:13 +0300

    VueJS. Stilizare conditionata

---
+++ public/4. Framework-uri/3. VueJS/5. Stilizare conditionata/app.js
@@ -0,0 +1,26 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+       number: 0
+    },
+    computed: {
+       color(){
+          if(this.number > 0){
+             return "green";
+          }
+          if(this.number < 0){
+            return "red";
+         }
+         return "yellow";
+       },
+       alertType(){
+         if(this.number > 0){
+            return "green";
+         }
+         if(this.number < 0){
+           return "red";
+        }
+        return "yellow";
+      }
+    }
+});
